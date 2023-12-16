02c1277747868aef32a199c36e65bb933bd718b8
anhlocok123sa1 <100480904+anhlocok123sa1@users.noreply.github.com>
2023-12-16 22:46:23 +0700

    version 1

---
+++ gioithieu.php
@@ -0,0 +1,17 @@
+﻿<div id="gioithieu">
+	<h3>GIỚI THIỆU</h3>
+	<p>
+		&nbsp &nbsp &nbsp &nbsp Chào mừng bạn đến với trang web điện thoại LocLe - nguồn thông tin hàng đầu về công nghệ tại Đại học Công Nghệ Sài Gòn! Chúng tôi tự hào là nền tảng truyền thông độc đáo, nơi cung cấp những tin tức, đánh giá và thông tin chuyên sâu về các sản phẩm và xu hướng mới nhất trong lĩnh vực điện thoại di động.
+		<br><br>
+		&nbsp &nbsp &nbsp &nbsp Với đội ngũ biên tập viên tận tâm và có kiến thức sâu rộng về công nghệ, chúng tôi cam kết mang đến cho bạn những bài viết chất lượng và cập nhật, giúp bạn hiểu rõ hơn về thế giới công nghệ đang ngày càng phát triển. Chúng tôi không chỉ giới thiệu về những sản phẩm hàng đầu mà còn tập trung vào việc hướng dẫn sử dụng, chia sẻ các mẹo hay và đánh giá chân thực để bạn có thể đưa ra quyết định mua sắm thông minh nhất.
+		<br><br>
+		&nbsp &nbsp &nbsp &nbsp Ngoài ra, trang web của chúng tôi còn là nơi để cộng đồng sinh viên và đam mê công nghệ tại Đại học Công Nghệ Sài Gòn kết nối, trao đổi thông tin và chia sẻ kinh nghiệm. Bạn sẽ được tham gia vào những thảo luận sôi nổi, sự kiện và cơ hội độc đáo để mở rộng mạng lưới kết nối trong cộng đồng.
+		<br><br>
+		&nbsp &nbsp &nbsp &nbsp Hãy đồng hành cùng chúng tôi trên hành trình khám phá không ngừng của công nghệ di động. LocLe - nơi bạn tìm thấy sự thông tin đa chiều, độc đáo và thú vị!
+	</p>
+	<img src="img/stu.jpg" width="500">
+	<p style="margin-bottom:10px; padding-left:60px;">Hình ảnh cửa hàng Điện Thoại Loc Le - 180 Cao Lo</p>
+	<h4>TP Hồ Chí Minh</h4>
+
+	<p>Địa chỉ : 180 Cao Lỗ, Quận 8  | Tel: 0794740393 </p>
+</div>
